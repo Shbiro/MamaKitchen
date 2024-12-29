8a213c16884c9da998ab08196f6d3639ebe9fe50
Amir Atias Hori <amiratiashuri@gmail.com>
2024-12-29 18:38:46 +0200

    כך בוצע העדכון וכו'

--- ReadMe2.py
+++ ReadMe2.py
@@ -6,54 +6,15 @@
 
 
 #התחברות ל ssh
-#ssh -i "Amir.pem" ec2-user@ec2-51-20-244-254.eu-north-1.compute.amazonaws.com
-
-#העלה לפרודקשן
-#scp -i C:\Users\USER\source\repos\amir.pem -r build C:\Users\USER\source\repos\BoMainAmir\Server.js ec2-user@ec2-51-20-244-254.eu-north-1.compute.amazonaws.com:~/BoMainAmir
-
-
-#שלב ראשון
-#git init
-#זה מייצר ריפוזיטורי
-
-
-#שלב שני
-
-#הוספת הקבצים למעקב על ידי git
+#ssh -i "amir.pem" ec2-user@13.60.120.78
 
 # git add .
 
 # ביצוע שינויים כולל הערה על מה היה השינוי
 
-# git commit - m "כך בוצע העדכון וכו'"
-
-#git remote add origin https://github.com/username/repositoryname.git
+# git commit -m "כך בוצע העדכון וכו'"
 
 # זה אם אני צריך remote
 
 # העלה ראשונה
 #git push -u origin main
-
-
-# לייצר תיקיית .gitignore
-
-# הוספת מספר דברים לקובץ הזה שאני רוצה שהוא לא יעלה לgit
-# ReadmyPrivate.py
-#node_modules/
-#.env
-#Amir.pem
-
-
-# הוספת הקובץ ignore
-
-#git add .gitignore
-#git commit -m "הוספת שינוי בקובץ Server.js"
-
-
-# העלה השינוים
-
-# git push
-
-# לאחר ביצוע שינויים הוספה למעקב 
-
-#git add .
--- server.js
+++ server.js
@@ -1,18 +1,27 @@
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 443; // פורט HTTPS
 
-// הגדרת המיקום של הקבצים של frontend
+// הגדרת תעודות SSL
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/13.60.120.78/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/13.60.120.78/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/13.60.120.78/chain.pem')
+};
+
+// הגדרת המיקום של קבצי ה-Frontend
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-// כל בקשה שתתבצע לא תואמת בנתיבים הקיימים תעבור לעמוד index.html
+// כל בקשה שאינה תואמת נתיבים תעבור לעמוד index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
-// עדכון הכתובת ל-0.0.0.0
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${port}`);
+// הפעלת השרת עם HTTPS
+https.createServer(options, app).listen(port, '0.0.0.0', () => {
+  console.log(`Server is running securely on https://13.60.120.78`);
 });
