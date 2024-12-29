8dfd66519bbd2d902c9bd5a1bc30231d7f0a4e8b
Amir Atias Hori <amiratiashuri@gmail.com>
2024-12-29 18:54:09 +0200

    Updated server.js to run on localhost and production

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
-const https = require('https');
-const fs = require('fs');
 const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 443; // פורט HTTPS
+const port = process.env.PORT || 5000;
 
-// הגדרת תעודות SSL
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/13.60.120.78/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/13.60.120.78/cert.pem'),
-  ca: fs.readFileSync('/etc/letsencrypt/live/13.60.120.78/chain.pem')
-};
-
-// הגדרת המיקום של קבצי ה-Frontend
+// הגדרת המיקום של הקבצים של frontend
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-// כל בקשה שאינה תואמת נתיבים תעבור לעמוד index.html
+// כל בקשה שתתבצע לא תואמת בנתיבים הקיימים תעבור לעמוד index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
-// הפעלת השרת עם HTTPS
-https.createServer(options, app).listen(port, '0.0.0.0', () => {
-  console.log(`Server is running securely on https://13.60.120.78`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
